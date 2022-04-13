fed34da3352d332f784c709ebb1ab1bc84802b5a
efiy <tsega599@gmail.com>
2022-04-12 22:38:12 -0700

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,19 @@
 #include "main.h"
 /**
-* main - Entry point
+* main - Prints Holberton
 *
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-char *sh = "Holberton";
-while (*sh)
-{
-_putchar(*sh);
-sh++;
-}
+_putchar('-');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
